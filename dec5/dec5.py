ed42740efc6bf644ae135809bc33d57c73755782
kschlesi <kschlesi42@gmail.com>
2024-12-09 22:27:52 -0800

    dec5 cleanup

--- dec5/dec5.py
+++ dec5/dec5.py
@@ -13,28 +13,20 @@ def check_output(example, rules):
     return 0
 
 if __name__ == "__main__":
+
     # read data
     rules = {}
     examples = []
-    # number_set = []
     with open('input.txt','r') as f:
         for line in f:
             examples.append(line.strip().split(',')) if ',' in line else rules.update({line.strip(): True})
     
-    # build number set and flatten
-    # number_set = [num for rule in rules.keys() for num in rule.split('|') if num != '']
-
     # finalize rules: add reverse of each rule to rules
     reverse_rules = {}
     [reverse_rules.update({reverse_rule(rule): False}) for rule in rules]
     rules.update(reverse_rules)
     
-    # print(rules)
-    # print(number_set)
-    # print(examples)
-    
     # check each example using rules
-    # REMOVE elements not in number_set
     output = [check_output(example, rules) for example in examples]
     print(output)
     print(sum(output))
